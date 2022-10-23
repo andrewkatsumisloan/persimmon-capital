e8d1a955a280c0ffd81db06393af1304494f6a70
andrewksloan <andrewksloan3@gmail.com>
2022-10-22 20:43:43 -0700

    Mobile formatting.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import MainFeed from "./Components/MainFeed/MainFeed";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <div className="main-container">
+          <div className="radial-gradient-test">Persimmon Capital</div>
+          <MainFeed></MainFeed>
+        </div>
+      </div>
+    </ChakraProvider>
   );
 }
 
